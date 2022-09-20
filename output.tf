c71cfdbaa2a1b498e59bf352eebf0dd533dac81f
Jeffrey Monte <46807283+jmonte-sph@users.noreply.github.com>
2022-09-20 17:45:26 +0800

    added output for deployer role

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "cf_execution_role_arn" {
   description = "Cloudformation Execution Role that will be used by serverless"
   value       = module.cf_execution_assume_role.iam_role_arn
 }
+
+output "deployer_role_arn" {
+  description = "Deployer Role that Github Actions will assume"
+  value       = module.github_actions_repo.role
+}
